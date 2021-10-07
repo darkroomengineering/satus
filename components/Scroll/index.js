4bd74641279cd1413f9cd4c5598622f14bf88143
clementroche <rchclement@gmail.com>
2021-10-07 19:22:08 +0200

    tips get scroll position

--- components/Scroll/index.js
+++ components/Scroll/index.js
@@ -1,23 +1,33 @@
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
-import { createContext, useLayoutEffect, useRef } from 'react'
+import {
+  createContext,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react'
 import { raf } from '@react-spring/rafz'
 import { useIsTouchDevice } from 'hooks/useIsTouchDevice'
+import { useDebounce, useMeasure } from 'react-use'
 
 export const ScrollContext = createContext(null)
 
 export const Scroll = ({ children }) => {
   const el = useRef()
+  const [ref, { width, height }] = useMeasure()
+
   const scroll = useRef()
+  const [isReady, setIsReady] = useState(false)
 
   const isTouchDevice = useIsTouchDevice()
 
   const router = useRouter()
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     const onRouteChangeComplete = () => {
-      scroll.current.setScroll(0, 0)
-      scroll.current.update()
+      scroll.current?.setScroll(0, 0)
+      scroll.current?.update()
     }
 
     router.events.on('routeChangeComplete', onRouteChangeComplete)
@@ -34,6 +44,7 @@ export const Scroll = ({ children }) => {
 
   useLayoutEffect(() => {
     async function initScroll() {
+      setIsReady(false)
       const LocomotiveScroll = (
         await import('@studio-freight/locomotive-scroll')
       ).default
@@ -49,6 +60,7 @@ export const Scroll = ({ children }) => {
         },
         autoRaf: false,
       })
+      setIsReady(true)
     }
 
     if (isTouchDevice !== undefined) {
@@ -64,11 +76,23 @@ export const Scroll = ({ children }) => {
     }
   }, [isTouchDevice])
 
-  useLayoutEffect(() => {}, [])
+  useDebounce(
+    () => {
+      scroll.current?.update()
+    },
+    1000,
+    [width, height]
+  )
 
   return (
-    <ScrollContext.Provider value={{ scroll: scroll.current }}>
-      <div ref={el} data-scroll-container>
+    <ScrollContext.Provider value={{ scroll: scroll.current, isReady }}>
+      <div
+        ref={(node) => {
+          el.current = node
+          ref(node)
+        }}
+        data-scroll-container
+      >
         {children}
       </div>
     </ScrollContext.Provider>
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,5 +1,24 @@
+import { ScrollContext } from 'components/Scroll'
+import { useContext, useEffect, useLayoutEffect } from 'react'
 import s from './style.module.scss'
 
 export default function Home() {
+  const { scroll, isReady } = useContext(ScrollContext)
+
+  // TIPS: get scroll position
+  // useEffect(() => {
+  //   if (!scroll) return
+
+  //   const onScroll = (e) => {
+  //     console.log(e)
+  //   }
+
+  //   scroll.on('scroll', onScroll)
+
+  //   return () => {
+  //     scroll.off('scroll', onScroll)
+  //   }
+  // }, [scroll, isReady])
+
   return <div className={s.pageHome}></div>
 }

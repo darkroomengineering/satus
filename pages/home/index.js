b7abc809ec2b5f111191ac42ac5b1bf61c9df1bc
clementroche <rchclement@gmail.com>
2021-10-12 00:39:43 +0200

    marquee

--- pages/home/index.js
+++ pages/home/index.js
@@ -4,6 +4,7 @@ import s from './style.module.scss'
 import { raf } from '@react-spring/rafz'
 import { useStore } from 'lib/store'
 import { Accordion } from 'components/Accordion'
+import { Marquee } from 'components/Marquee'
 
 export default function Home() {
   function update() {
@@ -21,6 +22,12 @@ export default function Home() {
 
   return (
     <div className={s.pageHome}>
+      <Marquee className={s.pageHome__marquee} offset={50} repeat={3}>
+        marquee stuff that scroll continuously
+      </Marquee>
+      <Marquee className={s.pageHome__marquee} inverted repeat={3}>
+        marquee stuff that scroll continuously
+      </Marquee>
       <Accordion.Group maxAccordionsOpenSimultaniously={3}>
         {Array(6)
           .fill({ header: 'this is header', body: 'this is body' })

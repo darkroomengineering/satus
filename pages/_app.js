606b7712fd0f1405ccd2d6314e640031fdf6ea15
clementroche <rchclement@gmail.com>
2021-10-14 17:49:13 +0200

    theme

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,18 @@ import { Header } from 'components/header'
 import { RealViewport } from 'components/real-viewport'
 import { Cursor } from 'components/cursor'
 import { useIsTouchDevice } from 'hooks/use-is-touch-device'
+import { useMediaQuery } from 'hooks/use-media-query'
+import { useLayoutEffect } from 'react'
+import { useStore } from 'lib/store'
 
 function MyApp({ Component, pageProps }) {
   const isTouchDevice = useIsTouchDevice()
+  const isDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const { setTheme } = useStore((state) => ({ setTheme: state.setTheme }))
+
+  useLayoutEffect(() => {
+    setTheme(isDarkMode ? 'dark' : 'light')
+  }, [isDarkMode])
 
   return (
     <>

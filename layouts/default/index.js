a77896f96212cc31812d78a1151654929e7ef47d
clementroche <rchclement@gmail.com>
2021-10-19 17:29:29 +0200

    scroll no ssr

--- layouts/default/index.js
+++ layouts/default/index.js
@@ -1,14 +1,27 @@
-import { Scroll } from 'components/scroll'
+// import { Scroll } from 'components/scroll'
 import { Footer } from 'components/footer'
 import s from './layout.module.scss'
+import dynamic from 'next/dynamic'
+import { useMemo } from 'react'
+
+const Scroll = dynamic(
+  () => import('components/scroll').then((mod) => mod.Scroll),
+  {
+    ssr: false,
+  }
+)
 
 export function Layout({ children }) {
-  return (
-    <>
-      <Scroll>
+  const content = useMemo(() => {
+    return (
+      <>
         <main className={s.main}>{children}</main>
         <Footer />
-      </Scroll>
-    </>
+      </>
+    )
+  }, [children])
+
+  return (
+    <>{typeof window !== 'undefined' ? <Scroll>{content}</Scroll> : content}</>
   )
 }

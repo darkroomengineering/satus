5eee0da1385e9e4c1187a45913259deb6316c568
clementroche <rchclement@gmail.com>
2021-10-19 22:04:49 +0200

    rollback scroll

--- components/scroll/index.js
+++ components/scroll/index.js
@@ -11,7 +11,6 @@ import { raf } from '@react-spring/rafz'
 import { useIsTouchDevice } from 'hooks/use-is-touch-device'
 import { useDebounce, useMeasure } from 'react-use'
 import { useStore } from 'lib/store'
-import LocomotiveScroll from '@studio-freight/locomotive-scroll'
 
 export const ScrollContext = createContext(null)
 
@@ -52,11 +51,11 @@ export const Scroll = ({ children }) => {
       setScroll(e)
     }
 
-    function initScroll() {
+    async function initScroll() {
       setIsReady(false)
-      // const LocomotiveScroll = (
-      //   await import('@studio-freight/locomotive-scroll')
-      // ).default
+      const LocomotiveScroll = (
+        await import('@studio-freight/locomotive-scroll')
+      ).default
 
       scroll.current = new LocomotiveScroll({
         el: el.current,
--- layouts/default/index.js
+++ layouts/default/index.js
@@ -1,27 +1,14 @@
-// import { Scroll } from 'components/scroll'
+import { Scroll } from 'components/scroll'
 import { Footer } from 'components/footer'
 import s from './layout.module.scss'
-import dynamic from 'next/dynamic'
-import { useMemo } from 'react'
-
-const Scroll = dynamic(
-  () => import('components/scroll').then((mod) => mod.Scroll),
-  {
-    ssr: false,
-  }
-)
 
 export function Layout({ children }) {
-  const content = useMemo(() => {
-    return (
-      <>
+  return (
+    <>
+      <Scroll>
         <main className={s.main}>{children}</main>
         <Footer />
-      </>
-    )
-  }, [children])
-
-  return (
-    <>{typeof window !== 'undefined' ? <Scroll>{content}</Scroll> : content}</>
+      </Scroll>
+    </>
   )
 }

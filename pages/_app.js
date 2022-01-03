be04bc5581cbf1c0278f50b26288c07f5b8bb00a
clementroche <rchclement@gmail.com>
2022-01-03 19:23:16 +0100

    dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import { RealViewport } from 'components/real-viewport'
 import { useDebug } from 'hooks/use-debug'
 import { useIsTouchDevice } from 'hooks/use-is-touch-device'
-import { useMediaQuery } from 'hooks/use-media-query'
 import { useStore } from 'lib/store'
 import dynamic from 'next/dynamic'
-import { useLayoutEffect } from 'react'
 import 'resize-observer-polyfill'
 import 'styles/global.scss'
+import useDarkMode from 'use-dark-mode'
 
 const Stats = dynamic(
   () => import('components/stats').then(({ Stats }) => Stats),
@@ -21,12 +20,8 @@ const GridDebugger = dynamic(
 
 function MyApp({ Component, pageProps }) {
   const isTouchDevice = useIsTouchDevice()
-  const isDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const darkMode = useDarkMode()
   const setTheme = useStore((state) => state.setTheme)
-
-  useLayoutEffect(() => {
-    setTheme(isDarkMode ? 'dark' : 'light')
-  }, [isDarkMode])
 
   const debug = useDebug()
 
---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+/* eslint-disable @next/next/no-document-import-in-page */
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class _Document extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charset="UTF-8" />
+          {/* <link
+            href="/fonts/aeonik/AeonikPro-Bold.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          /> */}
+        </Head>
+        <body>
+          {/* // https://github.com/donavon/use-dark-mode */}
+          {/* <script src="./noflash.js" /> */}
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default _Document

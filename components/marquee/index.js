913d58cff15365221f41d8214867f5e5c8bf39ef
Clément Roche <rchclement@gmail.com>
2022-11-22 11:35:24 +0100

    pause marquee when not intersecting

--- components/marquee/index.js
+++ components/marquee/index.js
@@ -1,7 +1,9 @@
 import cn from 'clsx'
+import { useRef } from 'react'
+import { useIntersection } from 'react-use'
 import s from './marquee.module.scss'
 
-export function Marquee({
+const Marquee = ({
   children,
   repeat = 2,
   duration = 5,
@@ -9,14 +11,27 @@ export function Marquee({
   inverted = false,
   className,
   animationStart = true,
-}) {
+  ...props
+}) => {
+  const ref = useRef()
+
+  const intersection = useIntersection(ref, { threshold: 0 })
+
   return (
     <div
-      className={cn(className, s.marquee, inverted && s.inverted)}
+      ref={ref}
+      {...props}
+      className={cn(
+        className,
+        s.marquee,
+        inverted && s.inverted,
+        intersection?.isIntersecting && 'running'
+      )}
       style={{
         '--duration': duration + 's',
         '--offset': (offset % 100) + '%',
-        '--animation-status': animationStart ? 'running' : 'paused',
+        '--animation-status':
+          intersection?.isIntersecting && animationStart ? 'running' : 'paused',
       }}
     >
       {new Array(repeat).fill(children).map((_, i) => (
@@ -27,3 +42,5 @@ export function Marquee({
     </div>
   )
 }
+
+export { Marquee }

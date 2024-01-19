0e62f7446fa0e04279199eabfa136ecee1329e24
arzafran <franco@studiofreight.com>
2024-01-19 16:30:42 -0300

    remove unused stuff

--- app/layout.js
+++ app/layout.js
@@ -18,27 +18,12 @@ export default function RootLayout({ children }) {
         <meta name="geo.region" content="US" />
 
         {/* START FAVICON */}
-        <link rel="manifest" href="/site.webmanifest" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
         {/* END FAVICON */}
+
       </head>
       <body suppressHydrationWarning>
         <RealViewport />

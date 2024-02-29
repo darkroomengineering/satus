d78f62410d67351a316726bdc79f84ee530ae0d0
Clément Roche <rchclement@gmail.com>
2024-02-29 13:02:20 +0100

    wrapper options

--- app/(pages)/wrapper/index.js
+++ app/(pages)/wrapper/index.js
@@ -4,11 +4,17 @@ import { Canvas } from 'libs/webgl/components/canvas'
 import { Footer } from '../footer'
 import s from './wrapper.module.scss'
 
-export function Wrapper({ children, theme = 'light', className }) {
+export function Wrapper({
+  children,
+  theme = 'light',
+  lenisOptions = {},
+  webgl = false,
+  className,
+}) {
   return (
     <>
-      <Lenis root />
-      <Canvas root />
+      <Lenis root options={lenisOptions} />
+      {webgl && <Canvas root />}
       <div className={cn(s.wrapper, `theme-${theme}`, className)}>
         {/* <Header /> */}
         <main role="main" className={s.main}>

9834f24851346dc426da14a6a97428fda6e36631
arzafran <franco@darkroom.engineering>
2024-05-28 16:18:29 -0300

    update github action to use node20

--- app/sw.js
+++ app/sw.js
@@ -1,12 +1,40 @@
-import { defaultCache } from '@serwist/next/worker'
 import { Serwist } from 'serwist'
+
+const DAYS_TO_CACHE = 10
 
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
-  runtimeCaching: defaultCache,
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'image-cache',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 24 * 60 * 60 * DAYS_TO_CACHE,
+        },
+      },
+    },
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'network-cache',
+        networkTimeoutSeconds: 10,
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 24 * 60 * 60, // Cache for 24 hours
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+  ],
 })
 
 serwist.addEventListeners()

8a3a5ff3c5ce1dfa08f2aa82ecfa9a77c8238873
Clément Roche <rchclement@gmail.com>
2024-07-17 01:06:36 +0200

    remove console log

--- app/(pages)/(components)/lenis/index.js
+++ app/(pages)/(components)/lenis/index.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { ReactLenis } from 'libs/lenis'
+import { ReactLenis } from 'lenis/react'
 
 export function Lenis({ root, options }) {
   return (
@@ -9,7 +9,6 @@ export function Lenis({ root, options }) {
       options={{
         ...options,
         prevent: (node) => {
-          console.log(node.id)
           return (
             node.nodeName === 'VERCEL-LIVE-FEEDBACK' ||
             node.id === 'theatrejs-studio-root'

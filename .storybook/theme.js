183306b6f54c0f333e307c90588fdff13f9ad4f6
Clément Roche <rchclement@gmail.com>
2024-07-29 16:50:50 +0200

    storybook logo

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+  brandTitle: 'Satūs',
+  brandUrl: 'https://satus.darkroom.engineering/',
+  brandImage: 'https://satus.darkroom.engineering/images/darkroom.svg',
+  brandTarget: '_self',
+})

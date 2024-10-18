9f9fce15fc7730a55ef29a8731aaf657ab549b44
feledori <felix@felixmayr.it>
2024-10-18 16:46:41 +0200

    remove unused prop

--- app/(pages)/r3f/page.jsx
+++ app/(pages)/r3f/page.jsx
@@ -7,12 +7,12 @@ import s from './r3f.module.css'
 export default function Home() {
   return (
     <TheatreProjectProvider id="Satus-R3f" config="/config/Satus-R3f.json">
-      <Wrapper theme="red" className={s.page} webgl={true}>
+      <Wrapper theme="red" className={s.page}>
         <div className={s.inner}>
           <Box className={s.box} />
         </div>
-        <Canvas root />
       </Wrapper>
+      <Canvas root />
     </TheatreProjectProvider>
   )
 }

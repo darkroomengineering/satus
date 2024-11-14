b70062ec829c7c12e5b0c32e25dec2ead6354929
Clément Roche <rchclement@gmail.com>
2024-11-14 17:51:48 +0100

    update lenis

--- app/(pages)/(components)/lenis/index.jsx
+++ app/(pages)/(components)/lenis/index.jsx
@@ -1,9 +1,9 @@
-"use client"
+'use client'
 
-import { useFrame } from "@darkroom.engineering/hamo"
-import "lenis/dist/lenis.css"
-import { ReactLenis } from "lenis/react"
-import { useRef } from "react"
+import { useFrame } from '@darkroom.engineering/hamo'
+import 'lenis/dist/lenis.css'
+import { ReactLenis } from 'lenis/react'
+import { useRef } from 'react'
 
 export function Lenis({ root, options }) {
   const lenisRef = useRef()
@@ -21,8 +21,8 @@ export function Lenis({ root, options }) {
         autoRaf: false,
         prevent: (node) => {
           return (
-            node.nodeName === "VERCEL-LIVE-FEEDBACK" ||
-            node.id === "theatrejs-studio-root"
+            node.nodeName === 'VERCEL-LIVE-FEEDBACK' ||
+            node.id === 'theatrejs-studio-root'
           )
         },
       }}

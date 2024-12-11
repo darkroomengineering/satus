93d9a1281120ecccc9abe408627e829c5560c770
Clément Roche <rchclement@gmail.com>
2024-12-11 13:33:01 +0100

    image component

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -1,13 +1,13 @@
-'use client'
+"use client"
 
-import cn from 'clsx'
-import NextImage, { type ImageProps as NextImageProps } from 'next/image'
-import type { CSSProperties, Ref } from 'react'
-import { breakpoints } from '~/styles/config.mjs'
-import s from './image.module.css'
+import cn from "clsx"
+import NextImage, { type ImageProps as NextImageProps } from "next/image"
+import type { CSSProperties, Ref } from "react"
+import { breakpoints } from "~/styles/config.mjs"
+import s from "./image.module.css"
 
-export type ImageProps = Omit<NextImageProps, 'objectFit' | 'alt'> & {
-  objectFit?: CSSProperties['objectFit']
+export type ImageProps = Omit<NextImageProps, "objectFit" | "alt"> & {
+  objectFit?: CSSProperties["objectFit"]
   block?: boolean
   mobileSize?: `${number}vw`
   desktopSize?: `${number}vw`
@@ -18,21 +18,24 @@ export type ImageProps = Omit<NextImageProps, 'objectFit' | 'alt'> & {
 export function Image({
   style,
   className,
-  loading = 'eager',
-  objectFit = 'cover',
+  loading = "eager",
+  objectFit = "cover",
   quality = 90,
-  alt = '',
-  block = true,
+  alt = "",
+  fill,
+  block = !fill,
   width = block ? 1 : undefined,
   height = block ? 1 : undefined,
-  mobileSize = '100vw',
-  desktopSize = '100vw',
+  mobileSize = "100vw",
+  desktopSize = "100vw",
   sizes,
   src,
   unoptimized,
   ref,
   ...props
 }: ImageProps) {
+  if (!src) return
+
   sizes =
     sizes || `(max-width: ${breakpoints.dt}px) ${mobileSize}, ${desktopSize}`
 
@@ -53,7 +56,7 @@ export function Image({
       sizes={sizes}
       src={src}
       unoptimized={
-        unoptimized || (typeof src === 'string' && src?.includes('.svg'))
+        unoptimized || (typeof src === "string" && src?.includes(".svg"))
       }
       draggable="false"
       onDragStart={(e) => e.preventDefault()}

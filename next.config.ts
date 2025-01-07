a066437b7a9904d5a62863081940084eff5f9326
arzafran <franco@darkroom.engineering>
2025-01-07 17:43:27 -0300

    fix svg import on build

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,67 @@ const nextConfig: NextConfig = {
     ],
     formats: ['image/avif', 'image/webp'],
   },
+  webpack: (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: { test?: { test?: (path: string) => boolean } }) =>
+        rule.test?.test?.('.svg')
+    )
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            memo: true,
+            dimensions: false,
+            svgoConfig: {
+              multipass: true,
+              plugins: [
+                'removeDimensions',
+                'removeOffCanvasPaths',
+                'reusePaths',
+                'removeElementsByAttr',
+                'removeStyleElement',
+                'removeScriptElement',
+                'prefixIds',
+                'cleanupIds',
+                {
+                  name: 'cleanupNumericValues',
+                  params: {
+                    floatPrecision: 1,
+                  },
+                },
+                {
+                  name: 'convertPathData',
+                  params: {
+                    floatPrecision: 1,
+                  },
+                },
+                {
+                  name: 'convertTransform',
+                  params: {
+                    floatPrecision: 1,
+                  },
+                },
+                {
+                  name: 'cleanupListOfValues',
+                  params: {
+                    floatPrecision: 1,
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    })
+
+    return config
+  },
   headers: async () => [
     {
       source: '/(.*)',

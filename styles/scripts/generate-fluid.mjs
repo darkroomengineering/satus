40af248ca124ffc4cdeaba3a5a5bc3f803d9df93
feledori <felix@felixmayr.it>
2025-02-04 11:57:10 +0100

    fix fluid calc

--- styles/scripts/generate-fluid.mjs
+++ styles/scripts/generate-fluid.mjs
@@ -72,7 +72,7 @@ function fluidUtility(name, properties) {
 	${propertiesArray
     .map(
       (property) =>
-        `${property}: calc((--value(integer) * 100) / var(--device-width));`
+        `${property}: calc((--value(integer) * 100) / var(--device-width) * 1vw);`
     )
     .join('\n')}
 }`

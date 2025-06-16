cb7d9c271dd44d5ce3f5aba6a5f37c274c17116d
Bucky Maler <maler.bucky@gmail.com>
2025-06-16 19:54:50 -0400

    support bundle analysis for turbopack

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  productionBrowserSourceMaps: process.env.SOURCE_MAPS === 'true',
   turbopack: {
     rules: {
       '*.svg': {

d9d5df027269d0fe0fb20aa7e641a789cd29cf8d
arzafran <franco@darkroom.engineering>
2025-08-21 09:54:19 -0300

    lint

--- integrations/mandrill/index.ts
+++ integrations/mandrill/index.ts
@@ -1,4 +1,4 @@
 // File: /Users/ivanfier/gitlab/Darkroom-Git/ael-website/integrations/mandrill/index.ts
-export { combinedContactAction, mandrillContactAction } from './action'
 
 export type { ContactData, MandrillConfig } from './action'
+export { combinedContactAction, mandrillContactAction } from './action'

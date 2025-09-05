fc73ecb42b02cf811c3822b4239ea368a182abc4
Clément Roche <rchclement@gmail.com>
2025-09-05 16:03:04 +0200

    webgl as wrapper prop

--- app/(pages)/r3f/page.jsx
+++ app/(pages)/r3f/page.jsx
@@ -1,17 +1,15 @@
 import { Wrapper } from '~/app/(pages)/(components)/wrapper'
 import { TheatreProjectProvider } from '~/orchestra/theatre'
-import { Canvas } from '~/webgl/components/canvas'
 import { Box } from './(components)/box'
 
 export default function Home() {
   return (
     <TheatreProjectProvider id="Satus-R3f" config="/config/Satus-R3f.json">
-      <Wrapper theme="red" className="font-mono uppercase">
+      <Wrapper theme="red" className="font-mono uppercase" webgl>
         <div className="flex items-center justify-center flex-grow">
           <Box className="size-[250px]" />
         </div>
       </Wrapper>
-      <Canvas root />
     </TheatreProjectProvider>
   )
 }

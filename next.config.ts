e918b45222d1d34a968f181de3e48b1d23b33492
arzafran <franco@darkroom.engineering>
2025-11-03 16:42:50 -0300

    rebunnyfy

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,8 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   reactCompiler: true,
   poweredByHeader: false,
-  productionBrowserSourceMaps: process.env.SOURCE_MAPS === 'true',
+  productionBrowserSourceMaps:
+    process.env.SOURCE_MAPS === 'true' && typeof Bun === 'undefined',
   typedRoutes: true,
   turbopack: {
     rules: {

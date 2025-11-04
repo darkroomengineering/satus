11be8f696436e1877cdce1d8c7ef9b5eec55d3d2
GFier <guido@darkroom.engineering>
2025-11-04 15:18:58 +0000

    sanity hotfix

--- app/studio/layout.tsx
+++ app/studio/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { headers } from 'next/headers'
+import { draftMode, headers } from 'next/headers'
 import type { PropsWithChildren } from 'react'
 
 export const metadata: Metadata = {
@@ -12,5 +12,14 @@ export const metadata: Metadata = {
 
 export default async function StudioLayout({ children }: PropsWithChildren) {
   await headers()
+
+  // Reload of entire app is happening only when entering the studio
+  // when draft mode is already enabled, we need to disable it
+  const { isEnabled: isDraftMode } = await draftMode()
+  if (isDraftMode) {
+    console.log('Disabling draft mode')
+    ;(await draftMode()).disable()
+  }
+
   return children
 }
--- integrations/sanity/components/disable-draft-mode.tsx
+++ integrations/sanity/components/disable-draft-mode.tsx
@@ -1,20 +1,27 @@
 'use client'
 
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { useDraftModeEnvironment } from 'next-sanity/hooks'
-import { Link } from '~/components/link'
 
 export function DisableDraftMode() {
   const environment = useDraftModeEnvironment()
+  const pathname = usePathname()
 
   // Only show the disable draft mode button when outside of Presentation Tool
   if (environment !== 'live' && environment !== 'unknown') {
     return null
   }
 
+  if (pathname.startsWith('/studio')) {
+    return null
+  }
+
   return (
     <Link
       href="/api/draft-mode/disable"
-      className="fixed right-safe top-safe bg-blue-500 text-primary uppercase px-4 py-2 rounded font-mono text-sm z-50"
+      scroll={false}
+      className="fixed right-safe top-safe bg-red text-primary uppercase dr-p-4 font-mono text-sm z-50"
     >
       Disable Draft Mode
     </Link>

c63d09292f65f4d905e5d77b75c4fecb5d98aeff
Clément Roche <rchclement@gmail.com>
2025-12-09 22:59:49 +0100

    imporve useDeviceDetection

--- hooks/use-device-detection.ts
+++ hooks/use-device-detection.ts
@@ -1,4 +1,5 @@
 import { useMediaQuery } from 'hamo'
+import { useEffect, useState } from 'react'
 import { breakpoints } from '~/styles/config'
 
 export function useDeviceDetection() {
@@ -7,12 +8,27 @@ export function useDeviceDetection() {
   const isMobile = useMediaQuery(`(max-width: ${breakpoint - 1}px)`)
   const isDesktop = useMediaQuery(`(min-width: ${breakpoint}px)`)
   const isReducedMotion = useMediaQuery('(prefers-reduced-motion: reduce)')
-  const isWebGL = isDesktop && !isReducedMotion
+  const isWebGL = isDesktop
+  const [dpr, setDpr] = useState<number | undefined>(undefined)
+  const [isSafari, setIsSafari] = useState<boolean | undefined>(undefined)
 
   // Check for low power mode with fallback for unsupported browsers
   const isLowPowerMode = useMediaQuery(
     '(any-pointer: coarse) and (hover: none)'
   )
 
-  return { isMobile, isDesktop, isReducedMotion, isWebGL, isLowPowerMode }
+  useEffect(() => {
+    setDpr(window.devicePixelRatio)
+    setIsSafari(/^((?!chrome|android).)*safari/i.test(navigator.userAgent))
+  }, [])
+
+  return {
+    isMobile,
+    isDesktop,
+    isReducedMotion,
+    isWebGL,
+    isLowPowerMode,
+    dpr,
+    isSafari,
+  }
 }
